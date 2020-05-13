9ed8b913d79c4249402d8f4767a4dcbd761600d6
Pavel Strunkin <pashidlos@gmail.com>
2020-05-13 22:04:42 +0200

    TestRunList moved to separate component

---
+++ src/components/TestRunList.tsx
@@ -0,0 +1,100 @@
+import React from "react";
+import {
+  Typography,
+  TableContainer,
+  TableHead,
+  TableCell,
+  Table,
+  TableRow,
+  TableBody,
+  IconButton,
+  Menu,
+  MenuItem,
+} from "@material-ui/core";
+import { MoreVert } from "@material-ui/icons";
+import { useHistory } from "react-router-dom";
+import { TestRun } from "../types";
+
+const TestRunList: React.FunctionComponent<{
+  items: TestRun[];
+  selectedId: string | undefined;
+  handleRemove: (id: string) => {};
+}> = ({ items, selectedId, handleRemove }) => {
+  const history = useHistory();
+  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+  const open = Boolean(anchorEl);
+
+  const handleClick = (event: React.MouseEvent<HTMLElement>) => {
+    event.stopPropagation();
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  return (
+    <TableContainer>
+      <Table>
+        <TableHead>
+          <TableRow>
+            <TableCell>Name</TableCell>
+            <TableCell>OS</TableCell>
+            <TableCell>Browser</TableCell>
+            <TableCell>Viewport</TableCell>
+            <TableCell>Status</TableCell>
+            <TableCell></TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {items.map((test) => (
+            <TableRow key={test.id} hover selected={test.id === selectedId}>
+              <TableCell
+                onClick={
+                  () => {
+                    history.push({
+                      search: `buildId=${test.buildId}&testId=${test.id}`,
+                    });
+                  }
+                  // () => history.push(`${routes.TEST_DETAILS_PAGE}/${test.id}`)
+                }
+              >
+                <Typography>{test.testVariation.name}</Typography>
+              </TableCell>
+              <TableCell>
+                <Typography>{test.testVariation.os}</Typography>
+              </TableCell>
+              <TableCell>
+                <Typography>{test.testVariation.browser}</Typography>
+              </TableCell>
+              <TableCell>
+                <Typography>{test.testVariation.viewport}</Typography>
+              </TableCell>
+              <TableCell>
+                <Typography>{test.status}</Typography>
+              </TableCell>
+              <TableCell>
+                <IconButton onClick={handleClick}>
+                  <MoreVert />
+                </IconButton>
+                <Menu anchorEl={anchorEl} open={open} onClose={handleClose}>
+                  <MenuItem>Details</MenuItem>
+                  <MenuItem
+                    onClick={() => {
+                      handleRemove(test.id);
+                      handleClose();
+                    }}
+                  >
+                    Delete
+                  </MenuItem>
+                </Menu>
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
+};
+
+export default TestRunList;
--- src/pages/ProjectPage.tsx
+++ src/pages/ProjectPage.tsx
@@ -1,25 +1,12 @@
 import React, { useEffect, useState } from "react";
-import {
-  Grid,
-  Typography,
-  TableContainer,
-  TableHead,
-  TableCell,
-  Table,
-  TableRow,
-  TableBody,
-  IconButton,
-  Menu,
-  MenuItem,
-} from "@material-ui/core";
-import { MoreVert } from "@material-ui/icons";
-import { useParams, useHistory, useLocation } from "react-router-dom";
+import { Grid } from "@material-ui/core";
+import { useParams, useLocation } from "react-router-dom";
 import { Build, TestRun } from "../types";
 import { projectsService, buildsService, testsService } from "../services";
-import { routes } from "../constants";
 import BuildList from "../components/BuildList";
 import ProjectSelect from "../components/ProjectSelect";
 import qs from "qs";
+import TestRunList from "../components/TestRunList";
 
 const getQueryParams = (guery: string) => {
   const queryParams = qs.parse(guery, { ignoreQueryPrefix: true });
@@ -30,14 +17,12 @@ const getQueryParams = (guery: string) => {
 };
 
 const ProjectPage = () => {
-  const history = useHistory();
   const { projectId } = useParams();
   const location = useLocation();
   const [builds, setBuilds] = useState<Build[]>([]);
-  const [tests, setTests] = useState<TestRun[]>([]);
+  const [testRuns, setTestRuns] = useState<TestRun[]>([]);
   const [selectedBuildId, setSelectedBuildId] = useState<string>();
-  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
-  const open = Boolean(anchorEl);
+  const [selectedTestdId, setSelectedTestId] = useState<string>();
 
   useEffect(() => {
     const queryParams = getQueryParams(location.search);
@@ -45,6 +30,11 @@ const ProjectPage = () => {
       setSelectedBuildId(queryParams.buildId);
     } else if (builds.length > 0) {
       setSelectedBuildId(builds[0].id);
+    }
+    if (queryParams.testId) {
+      setSelectedTestId(queryParams.testId);
+    } else {
+      setSelectedTestId(undefined)
     }
   }, [location.search, builds]);
 
@@ -57,19 +47,10 @@ const ProjectPage = () => {
   useEffect(() => {
     if (selectedBuildId) {
       buildsService.getTestRuns(selectedBuildId).then((testRuns: TestRun[]) => {
-        setTests(testRuns);
+        setTestRuns(testRuns);
       });
     }
   }, [selectedBuildId]);
-
-  const handleClick = (event: React.MouseEvent<HTMLElement>) => {
-    event.stopPropagation();
-    setAnchorEl(event.currentTarget);
-  };
-
-  const handleClose = () => {
-    setAnchorEl(null);
-  };
 
   return (
     <Grid container>
@@ -91,74 +72,17 @@ const ProjectPage = () => {
         <Grid container direction="column">
           <Grid item>Pannel</Grid>
           <Grid item>
-            <TableContainer>
-              <Table>
-                <TableHead>
-                  <TableRow>
-                    <TableCell>Name</TableCell>
-                    <TableCell>OS</TableCell>
-                    <TableCell>Browser</TableCell>
-                    <TableCell>Viewport</TableCell>
-                    <TableCell>Status</TableCell>
-                    <TableCell></TableCell>
-                  </TableRow>
-                </TableHead>
-                <TableBody>
-                  {tests.map((test) => (
-                    <TableRow key={test.id} hover>
-                      <TableCell
-                        onClick={() =>
-                          history.push(`${routes.TEST_DETAILS_PAGE}/${test.id}`)
-                          // history.push({
-                          //   search: "testId=" + test.id,
-                          // })
-                        }
-                      >
-                        <Typography>{test.testVariation.name}</Typography>
-                      </TableCell>
-                      <TableCell>
-                        <Typography>{test.testVariation.os}</Typography>
-                      </TableCell>
-                      <TableCell>
-                        <Typography>{test.testVariation.browser}</Typography>
-                      </TableCell>
-                      <TableCell>
-                        <Typography>{test.testVariation.viewport}</Typography>
-                      </TableCell>
-                      <TableCell>
-                        <Typography>{test.status}</Typography>
-                      </TableCell>
-                      <TableCell>
-                        <IconButton onClick={handleClick}>
-                          <MoreVert />
-                        </IconButton>
-                        <Menu
-                          anchorEl={anchorEl}
-                          open={open}
-                          onClose={handleClose}
-                        >
-                          <MenuItem>Details</MenuItem>
-                          <MenuItem
-                            onClick={() => {
-                              testsService.remove(test.id).then((isRemoved) => {
-                                if (isRemoved) {
-                                  setTests(
-                                    tests.filter((item) => item.id !== test.id)
-                                  );
-                                }
-                                handleClose();
-                              });
-                            }}
-                          >
-                            Delete
-                          </MenuItem>
-                        </Menu>
-                      </TableCell>
-                    </TableRow>
-                  ))}
-                </TableBody>
-              </Table>
-            </TableContainer>
+            <TestRunList
+              items={testRuns}
+              selectedId={selectedTestdId}
+              handleRemove={(id: string) =>
+                testsService.remove(id).then((isRemoved) => {
+                  if (isRemoved) {
+                    setTestRuns(testRuns.filter((item) => item.id !== id));
+                  }
+                })
+              }
+            />
           </Grid>
         </Grid>
       </Grid>
