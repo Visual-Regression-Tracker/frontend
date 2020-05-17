28e10b0beb7045b214ae69d968edccf2fa705018
Pavel Strunkin <pashidlos@gmail.com>
2020-05-17 11:45:25 +0200

    ProjectList page styled

--- src/pages/ProjectListPage.tsx
+++ src/pages/ProjectListPage.tsx
@@ -14,6 +14,9 @@ import {
   TextField,
   DialogActions,
   Button,
+  Container,
+  Box,
+  useTheme,
 } from "@material-ui/core";
 import {
   useProjectState,
@@ -26,6 +29,7 @@ import { Link } from "react-router-dom";
 import { Delete, Add } from "@material-ui/icons";
 
 const ProjectsListPage = () => {
+  const theme = useTheme();
   const projectState = useProjectState();
   const projectDispatch = useProjectDispatch();
 
@@ -45,77 +49,95 @@ const ProjectsListPage = () => {
   };
 
   return (
-    <Grid container direction="column" spacing={2}>
-      <Grid item>
-        <Fab color="primary" aria-label="add" onClick={handleCreateClickOpen}>
-          <Add />
-        </Fab>
-        <Dialog
-          open={createDialogOpen}
-          onClose={handleCreateClose}
-          aria-labelledby="form-dialog-title"
-        >
-          <DialogTitle id="form-dialog-title">Create Project</DialogTitle>
-          <DialogContent>
-            <TextField
-              autoFocus
-              margin="dense"
-              id="name"
-              label="Project name"
-              type="text"
-              fullWidth
-              value={newProjectName}
-              onChange={(event) => setNewProjectName(event.target.value)}
-            />
-          </DialogContent>
-          <DialogActions>
-            <Button onClick={handleCreateClose} color="primary">
-              Cancel
-            </Button>
-            <Button
-              onClick={() => {
-                createProject(projectDispatch, {
-                  name: newProjectName,
-                }).then((project) => {
-                  setNewProjectName("");
-                  handleCreateClose();
-                });
-              }}
+    <Container
+      style={{
+        marginTop: theme.spacing(4),
+      }}
+    >
+      <Grid container spacing={2}>
+        <Grid item xs={4}>
+          <Box
+            height="100%"
+            alignItems="center"
+            justifyContent="center"
+            display="flex"
+          >
+            <Fab
               color="primary"
+              aria-label="add"
+              onClick={handleCreateClickOpen}
             >
-              Create
-            </Button>
-          </DialogActions>
-        </Dialog>
-      </Grid>
-      {projectState.projectList.map((project) => (
-        <Grid item key={project.id}>
-          <Card>
-            <CardContent>
-              <Typography>Key: {project.id}</Typography>
-            </CardContent>
-            <CardActionArea
-              component={Link}
-              to={`${project.id}`}
-            >
-              <CardContent>
-                <Typography>Name: {project.name}</Typography>
-                <Typography>Updated: {project.updatedAt}</Typography>
-              </CardContent>
-            </CardActionArea>
-            <CardActions>
-              <IconButton
-                onClick={(event: React.MouseEvent<HTMLElement>) => {
-                  deleteProject(projectDispatch, project.id);
+              <Add />
+            </Fab>
+          </Box>
+
+          <Dialog
+            open={createDialogOpen}
+            onClose={handleCreateClose}
+            aria-labelledby="form-dialog-title"
+          >
+            <DialogTitle id="form-dialog-title">Create Project</DialogTitle>
+            <DialogContent>
+              <TextField
+                autoFocus
+                margin="dense"
+                id="name"
+                label="Project name"
+                type="text"
+                fullWidth
+                value={newProjectName}
+                onChange={(event) => setNewProjectName(event.target.value)}
+              />
+            </DialogContent>
+            <DialogActions>
+              <Button onClick={handleCreateClose} color="primary">
+                Cancel
+              </Button>
+              <Button
+                onClick={() => {
+                  createProject(projectDispatch, {
+                    name: newProjectName,
+                  }).then((project) => {
+                    setNewProjectName("");
+                    handleCreateClose();
+                  });
                 }}
+                color="primary"
               >
-                <Delete />
-              </IconButton>
-            </CardActions>
-          </Card>
+                Create
+              </Button>
+            </DialogActions>
+          </Dialog>
         </Grid>
-      ))}
-    </Grid>
+        {projectState.projectList.map((project) => (
+          <Grid item xs={4} key={project.id}>
+            <Card>
+              <CardContent>
+                <Typography>Key: {project.id}</Typography>
+              </CardContent>
+              <CardActionArea component={Link} to={`${project.id}`}>
+                <CardContent>
+                  <Typography>Name: {project.name}</Typography>
+                  <Typography>Updated: {project.updatedAt}</Typography>
+                </CardContent>
+              </CardActionArea>
+              <CardActions>
+                <IconButton
+                  onClick={(event: React.MouseEvent<HTMLElement>) => {
+                    deleteProject(projectDispatch, project.id);
+                  }}
+                  style={{
+                    marginLeft: "auto",
+                  }}
+                >
+                  <Delete />
+                </IconButton>
+              </CardActions>
+            </Card>
+          </Grid>
+        ))}
+      </Grid>
+    </Container>
   );
 };
 
