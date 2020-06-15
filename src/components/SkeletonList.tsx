556421ac2bed2d03311c9386a9b2df3b5e3ca9a7
Pavel Strunkin <pashidlos@gmail.com>
2020-06-15 22:08:49 +0200

    list warning fixed

--- src/components/SkeletonList.tsx
+++ src/components/SkeletonList.tsx
@@ -3,10 +3,11 @@ import { Box } from "@material-ui/core";
 import { Skeleton } from "@material-ui/lab";
 
 export const SkeletonList: React.FunctionComponent = () => {
+  const list = [1, 2, 3, 4, 5, 6, 7, 8, 9];
   return (
     <React.Fragment>
-      {[...Array(9)].map((i) => (
-        <Box p={0.5}>
+      {list.map((i) => (
+        <Box p={0.5} key={i}>
           <Skeleton variant="rect" height={80} />
         </Box>
       ))}
