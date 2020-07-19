9c9b399eb517145e585140efd6cae85897688014
Pavel Strunkin <pashidlos@gmail.com>
2020-07-19 14:21:12 +0200

    project related events are handeled

--- src/contexts/project.context.tsx
+++ src/contexts/project.context.tsx
@@ -1,7 +1,6 @@
 import * as React from "react";
 import { Project } from "../types";
 import { projectsService } from "../services";
-import { useAuthState } from "./auth.context";
 
 interface IRequestAction {
   type: "request";
@@ -83,13 +82,6 @@ function projectReducer(state: State, action: IAction): State {
 
 function ProjectProvider({ children }: ProjectProviderProps) {
   const [state, dispatch] = React.useReducer(projectReducer, initialState);
-  const { loggedIn } = useAuthState();
-
-  React.useEffect(() => {
-    if (loggedIn) {
-      getProjectList(dispatch);
-    }
-  }, [loggedIn]);
 
   return (
     <ProjectStateContext.Provider value={state}>
@@ -119,14 +111,9 @@ function useProjectDispatch() {
 async function getProjectList(dispatch: Dispatch) {
   dispatch({ type: "request" });
 
-  projectsService
-    .getAll()
-    .then((projects) => {
-      dispatch({ type: "get", payload: projects });
-    })
-    .catch((error) => {
-      console.log(error.toString());
-    });
+  return projectsService.getAll().then((projects) => {
+    dispatch({ type: "get", payload: projects });
+  });
 }
 
 async function createProject(
@@ -135,14 +122,10 @@ async function createProject(
 ) {
   dispatch({ type: "request" });
 
-  projectsService
-    .create(project)
-    .then((project: Project) => {
-      dispatch({ type: "create", payload: project });
-    })
-    .catch((error) => {
-      console.log(error.toString());
-    });
+  return projectsService.create(project).then((project: Project) => {
+    dispatch({ type: "create", payload: project });
+    return project;
+  });
 }
 
 async function updateProject(
@@ -151,29 +134,19 @@ async function updateProject(
 ) {
   dispatch({ type: "request" });
 
-  projectsService
-    .update(project)
-    .then((project: Project) => {
-      dispatch({ type: "update", payload: project });
-    })
-    .catch((error) => {
-      console.log(error.toString());
-    });
+  return projectsService.update(project).then((project: Project) => {
+    dispatch({ type: "update", payload: project });
+    return project;
+  });
 }
 
 async function deleteProject(dispatch: Dispatch, id: string) {
   dispatch({ type: "request" });
 
-  projectsService
-    .remove(id)
-    .then((isDeleted) => {
-      if (isDeleted) {
-        dispatch({ type: "delete", payload: id });
-      }
-    })
-    .catch((error) => {
-      console.log(error.toString());
-    });
+  return projectsService.remove(id).then((project) => {
+    dispatch({ type: "delete", payload: id });
+    return project;
+  });
 }
 
 export {
