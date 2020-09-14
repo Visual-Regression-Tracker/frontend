6ad13a44c9ac2181f14affe3040c6dc4d88f02d4
Pavel Strunkin <pashidlos@gmail.com>
2020-09-14 17:56:59 +0200

    git-branch added

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -1,6 +1,8 @@
 const {
   addVisualRegressionTrackerPlugin,
 } = require("@visual-regression-tracker/agent-cypress/dist/plugin");
+const branch = require('git-branch');
+
 
 module.exports = (on, config) => {
   require("cypress-react-unit-test/plugins/react-scripts")(on, config);
@@ -8,6 +10,14 @@ module.exports = (on, config) => {
   if (config.env.VRT_API_KEY) {
     config.env.visualRegressionTracker.apiKey = config.env.VRT_API_KEY;
   }
+  // try to get the current branch name to set it as branch name for VRT plugin
+  await branch('./')
+    .then((name) => {
+      // update the config with the retrieved name
+      // eslint-disable-next-line no-param-reassign
+      config.env.visualRegressionTracker.branchName = name;
+    });
+
   addVisualRegressionTrackerPlugin(on, config);
 
   return config;
