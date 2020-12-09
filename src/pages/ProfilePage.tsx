ec2ecf0223c22f75a5aa605c3b422c38c34f5c65
Pavel Strunkin <pashidlos@gmail.com>
2020-12-09 22:53:02 +0200

    Update ProfilePage.tsx

--- src/pages/ProfilePage.tsx
+++ src/pages/ProfilePage.tsx
@@ -18,6 +18,7 @@ import {
   update,
   selectProject,
   useProjectDispatch,
+  useProjectState,
 } from "../contexts";
 import { usersService } from "../services";
 import { useSnackbar } from "notistack";
@@ -28,6 +29,7 @@ const ProfilePage = () => {
   const { user } = useAuthState();
   const authDispatch = useAuthDispatch();
   const projectDispatch = useProjectDispatch();
+  const { selectedProjectId } = useProjectState();
   const [email, setEmail] = useState(user?.email);
   const [firstName, setFirstName] = useState(user?.firstName);
   const [lastName, setLastName] = useState(user?.lastName);
@@ -77,203 +79,225 @@ const ProfilePage = () => {
   };
 
   return (
-    <Grid container>
-      <Grid item xs={6}>
-        <Grid container>
-          <Grid item xs={12}>
-            <form onSubmit={handleUserUpdateSubmit}>
-              <Card variant="outlined">
-                <CardHeader title={"User details"} />
-                <CardContent>
-                  <Grid container spacing={2}>
-                    <Grid item xs={6}>
-                      <TextField
-                        id="firstName"
-                        name="firstName"
-                        value={firstName}
-                        label={"First name"}
-                        type="text"
-                        variant="outlined"
-                        required
-                        fullWidth
-                        inputProps={{
-                          onChange: (event) =>
-                            setFirstName(
-                              (event.target as HTMLInputElement).value
-                            ),
-                          "data-testid": "firstName",
-                        }}
-                      />
+    <Box m={2}>
+      <Grid container spacing={1}>
+        <Grid item xs={6}>
+          <Grid container spacing={1}>
+            <Grid item xs={12}>
+              <form onSubmit={handleUserUpdateSubmit}>
+                <Card variant="outlined">
+                  <CardHeader title={"User details"} />
+                  <CardContent>
+                    <Grid container spacing={2}>
+                      <Grid item xs={6}>
+                        <TextField
+                          id="firstName"
+                          name="firstName"
+                          value={firstName}
+                          label={"First name"}
+                          type="text"
+                          variant="outlined"
+                          required
+                          fullWidth
+                          inputProps={{
+                            onChange: (event) =>
+                              setFirstName(
+                                (event.target as HTMLInputElement).value
+                              ),
+                            "data-testid": "firstName",
+                          }}
+                        />
+                      </Grid>
+                      <Grid item xs={6}>
+                        <TextField
+                          id="lastName"
+                          name="lastName"
+                          value={lastName}
+                          label={"Last name"}
+                          type="text"
+                          variant="outlined"
+                          required
+                          fullWidth
+                          inputProps={{
+                            onChange: (event) =>
+                              setLastName(
+                                (event.target as HTMLInputElement).value
+                              ),
+                            "data-testid": "lastName",
+                          }}
+                        />
+                      </Grid>
+                      <Grid item xs={12}>
+                        <TextField
+                          id="email"
+                          name="email"
+                          value={email}
+                          label={"Email address"}
+                          type="text"
+                          variant="outlined"
+                          required
+                          fullWidth
+                          inputProps={{
+                            onChange: (event) =>
+                              setEmail(
+                                (event.target as HTMLInputElement).value
+                              ),
+                            "data-testid": "email",
+                          }}
+                        />
+                      </Grid>
                     </Grid>
-                    <Grid item xs={6}>
-                      <TextField
-                        id="lastName"
-                        name="lastName"
-                        value={lastName}
-                        label={"Last name"}
-                        type="text"
-                        variant="outlined"
-                        required
-                        fullWidth
-                        inputProps={{
-                          onChange: (event) =>
-                            setLastName(
-                              (event.target as HTMLInputElement).value
-                            ),
-                          "data-testid": "lastName",
-                        }}
-                      />
+                  </CardContent>
+                  <CardActions>
+                    <Grid container justify="center">
+                      <Grid item>
+                        <Button
+                          type="submit"
+                          color="primary"
+                          variant="outlined"
+                          data-testid="submit"
+                        >
+                          Update
+                        </Button>
+                      </Grid>
                     </Grid>
-                    <Grid item xs={12}>
-                      <TextField
-                        id="email"
-                        name="email"
-                        value={email}
-                        label={"Email address"}
-                        type="text"
-                        variant="outlined"
-                        required
-                        fullWidth
-                        inputProps={{
-                          onChange: (event) =>
-                            setEmail((event.target as HTMLInputElement).value),
-                          "data-testid": "email",
-                        }}
-                      />
+                  </CardActions>
+                </Card>
+              </form>
+            </Grid>
+            <Grid item xs={12}>
+              <form onSubmit={handlePasswordUpdateSubmit}>
+                <Card variant="outlined">
+                  <CardHeader title="Change password" />
+                  <CardContent>
+                    <Grid container spacing={2}>
+                      <Grid item xs={12}>
+                        <TextField
+                          id="password"
+                          name="password"
+                          value={password}
+                          label={"New password"}
+                          type="password"
+                          variant="outlined"
+                          required
+                          fullWidth
+                          inputProps={{
+                            onChange: (event) =>
+                              setPassword(
+                                (event.target as HTMLInputElement).value
+                              ),
+                            "data-testid": "password",
+                          }}
+                        />
+                      </Grid>
                     </Grid>
-                  </Grid>
-                </CardContent>
-                <CardActions>
-                  <Grid container justify="center">
-                    <Grid item>
-                      <Button
-                        type="submit"
-                        color="primary"
-                        variant="outlined"
-                        data-testid="submit"
-                      >
-                        Update
-                      </Button>
+                  </CardContent>
+                  <CardActions>
+                    <Grid container justify="center">
+                      <Grid item>
+                        <Button
+                          type="submit"
+                          color="primary"
+                          variant="outlined"
+                          data-testid="submit"
+                        >
+                          Update
+                        </Button>
+                      </Grid>
                     </Grid>
-                  </Grid>
-                </CardActions>
-              </Card>
-            </form>
-          </Grid>
-          <Grid item xs={12}>
-            <form onSubmit={handlePasswordUpdateSubmit}>
-              <Card variant="outlined">
-                <CardHeader title="Change password" />
-                <CardContent>
-                  <Grid container spacing={2}>
-                    <Grid item xs={12}>
-                      <TextField
-                        id="password"
-                        name="password"
-                        value={password}
-                        label={"New password"}
-                        type="password"
-                        variant="outlined"
-                        required
-                        fullWidth
-                        inputProps={{
-                          onChange: (event) =>
-                            setPassword(
-                              (event.target as HTMLInputElement).value
-                            ),
-                          "data-testid": "password",
-                        }}
-                      />
-                    </Grid>
-                  </Grid>
-                </CardContent>
-                <CardActions>
-                  <Grid container justify="center">
-                    <Grid item>
-                      <Button
-                        type="submit"
-                        color="primary"
-                        variant="outlined"
-                        data-testid="submit"
-                      >
-                        Update
-                      </Button>
-                    </Grid>
-                  </Grid>
-                </CardActions>
-              </Card>
-            </form>
+                  </CardActions>
+                </Card>
+              </form>
+            </Grid>
           </Grid>
         </Grid>
-      </Grid>
-      <Grid item xs={6}>
-        <Grid container>
-          <Grid item xs={12}>
-            <Card variant="outlined">
-              <CardHeader title={"Api key"} />
-              <CardContent>
-                <Typography>{user?.apiKey}</Typography>
-              </CardContent>
-            </Card>
-          </Grid>
-          <Grid item xs={12}>
-            <Card variant="outlined">
-              <CardHeader title={"Configuration examples"} />
-              <CardContent>
-                <ProjectSelect
-                  onProjectSelect={(id) => selectProject(projectDispatch, id)}
-                />
-                <Tabs
-                  value={tabIndex}
-                  indicatorColor="primary"
-                  textColor="primary"
-                  onChange={(
-                    event: React.ChangeEvent<{}>,
-                    newValue: number
-                  ) => {
-                    setTabIndex(newValue);
-                  }}
-                >
-                  <Tab label="Config file" />
-                  <Tab label="Environment variables" />
-                </Tabs>
-                <div role="tabpanel" hidden={tabIndex !== 0}>
-                  {tabIndex === 0 && (
-                    <Box p={3}>
-                      <Typography>Copy and save it as vrt.json</Typography>
-                      <TextField
-                        id="configFile"
-                        name="configFile"
-                        variant="outlined"
-                        disabled
-                        value={`{
-  "apiUrl": "${window._env_.REACT_APP_API_URL}",
-  "apiKey": "${user?.apiKey}",
-  "project": "project",
-  "branchName": "master",
-  "ciBuildId": "config_file",
-  "enableSoftAssert": false
+        <Grid item xs={6}>
+          <Grid container spacing={1}>
+            <Grid item xs={12}>
+              <Card variant="outlined">
+                <CardHeader title={"Api key"} />
+                <CardContent>
+                  <Typography>{user?.apiKey}</Typography>
+                </CardContent>
+              </Card>
+            </Grid>
+            <Grid item xs={12}>
+              <Card variant="outlined">
+                <CardHeader title={"Configuration examples"} />
+                <CardContent>
+                  <ProjectSelect
+                    onProjectSelect={(id) => selectProject(projectDispatch, id)}
+                  />
+                  <Tabs
+                    variant="fullWidth"
+                    value={tabIndex}
+                    indicatorColor="primary"
+                    textColor="primary"
+                    onChange={(
+                      event: React.ChangeEvent<{}>,
+                      newValue: number
+                    ) => {
+                      setTabIndex(newValue);
+                    }}
+                  >
+                    <Tab label="Config file" />
+                    <Tab label="ENV variables" />
+                  </Tabs>
+                  <div role="tabpanel" hidden={tabIndex !== 0}>
+                    {tabIndex === 0 && (
+                      <Box p={3}>
+                        <TextField
+                          id="configFile"
+                          name="configFile"
+                          variant="outlined"
+                          disabled
+                          value={`{
+    "apiUrl": "${window._env_.REACT_APP_API_URL}",
+    "apiKey": "${user?.apiKey}",
+    "project": "${selectedProjectId ?? "Default project"}",
+    "branchName": "master",
+    "ciBuildId": "commit_sha",
+    "enableSoftAssert": false
 }`}
-                        multiline
-                        rows={8}
-                        fullWidth
-                      />
-                    </Box>
-                  )}
-                </div>
-                <div role="tabpanel" hidden={tabIndex !== 1}>
-                  {tabIndex === 1 && (
-                    <Box p={3}>
-                      <Typography>Some config2</Typography>
-                    </Box>
-                  )}
-                </div>
-              </CardContent>
-            </Card>
+                          multiline
+                          rows={8}
+                          fullWidth
+                          helperText="Save as vrt.json in project root. Could be overridden with ENV variables"
+                        />
+                      </Box>
+                    )}
+                  </div>
+                  <div role="tabpanel" hidden={tabIndex !== 1}>
+                    {tabIndex === 1 && (
+                      <Box p={3}>
+                        <TextField
+                          id="envVariables"
+                          name="envVariables"
+                          variant="outlined"
+                          disabled
+                          value={`
+  VRT_APIURL="${window._env_.REACT_APP_API_URL}"
+  VRT_APIKEY="${user?.apiKey}"
+  VRT_PROJECT="${selectedProjectId ?? "Default project"} "
+  VRT_BRANCHNAME="master"
+  VRT_CIBUILDID="commit_sha"
+  VRT_ENABLESOFTASSERT=false
+  `}
+                          multiline
+                          rows={8}
+                          fullWidth
+                          helperText="Add Environment variables inside CI environment"
+                        />
+                      </Box>
+                    )}
+                  </div>
+                </CardContent>
+              </Card>
+            </Grid>
           </Grid>
         </Grid>
       </Grid>
-    </Grid>
+    </Box>
   );
 };
 
