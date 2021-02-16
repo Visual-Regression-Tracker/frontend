815288971b28119a6d0eb678107d5827bc7e3e1e
Pavlo Strunkin <pashidlos@gmail.com>
2021-02-17 00:03:56 +0200

    Switch to client instead of server side pagination https://github.com/Visual-Regression-Tracker/Visual-Regression-Tracker/issues/213

--- src/_test/stub.helper.ts
+++ src/_test/stub.helper.ts
@@ -3,14 +3,14 @@ import { buildsService, projectsService, testRunService } from "../services";
 import { Build, Project, TestRun } from "../types";
 
 const projectStub = {
-  getAll: (projects: Project[]) =>
+  getAll: (projects: Array<Project>) =>
     cy.stub(projectsService, "getAll").resolves(projects),
 };
 
 const buildsServiceStub = {
   getDetails: (build: Build) =>
     cy.stub(buildsService, "getDetails").resolves(build),
-  getList: (builds: Build[]) =>
+  getList: (builds: Array<Build>) =>
     cy.stub(buildsService, "getList").resolves({
       data: builds,
       total: 100,
@@ -20,10 +20,8 @@ const buildsServiceStub = {
 };
 
 const testRunServiceStub = {
-  getList: (testRuns: TestRun[]) =>
-    cy
-      .stub(testRunService, "getList")
-      .resolves({ data: testRuns, total: 100, skip: 0, take: 10 }),
+  getList: (testRuns: Array<TestRun>) =>
+    cy.stub(testRunService, "getList").resolves(testRuns),
 };
 
 export { projectStub, buildsServiceStub, testRunServiceStub };
