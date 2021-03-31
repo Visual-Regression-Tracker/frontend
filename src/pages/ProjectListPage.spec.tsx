16771a1e521de206946c450951a457b4ed031ae3
Pavel Strunkin <pashidlos@gmail.com>
2021-03-31 10:40:54 +0300

    Update ProjectListPage.spec.tsx

--- src/pages/ProjectListPage.spec.tsx
+++ src/pages/ProjectListPage.spec.tsx
@@ -1,11 +1,16 @@
 /* global cy */
 import React from "react";
 import ProjectListPage from "./ProjectListPage";
-import { PROJECT_ONE } from "../_test/test.data.helper";
+import { haveUserLogged } from "../_test/precondition.helper";
+import { PROJECT_ONE, userMock } from "../_test/test.data.helper";
 import { mountVrtComponent } from "../_test/test.moun.helper";
 import { projectStub } from "../_test/stub.helper";
 
 describe("Project List page", () => {
+  before(() => {
+    haveUserLogged(userMock);
+  });
+
   it("image", () => {
     projectStub.getAll([PROJECT_ONE]);
 
