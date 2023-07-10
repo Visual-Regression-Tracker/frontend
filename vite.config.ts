536529fc532d1fbf8af7c832b4add0a85008e0ca
Juga Paazmaya <paazmaya@yahoo.com>
2023-07-10 09:05:38 +0300

    Update Typescript and configure chunks for build

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,25 @@
 import {defineConfig} from "vite";
+
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
     build: {
-        outDir: 'build'
+        outDir: 'build',
+
+        // https://rollupjs.org/configuration-options/
+        rollupOptions: {
+            output: {
+                manualChunks: manualChunks
+            }
+        }
     }
 });
+function manualChunks(id) {
+    if (id.includes('node_modules/react')) {
+        return 'react';
+    } else if (id.includes('node_modules')) {
+        return 'vendor';
+    }
+}
