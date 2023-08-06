3c728b8375d719b89ce4adc53bc5d671a72c3c9b
Pavlo Strunkin <pashidlos@gmail.com>
2023-08-06 23:08:21 +0300

    change to macos

--- src/components/BuildStatusChip.test.tsx
+++ src/components/BuildStatusChip.test.tsx
@@ -1,7 +1,5 @@
 import React from "react";
-import { expect, test, describe } from "@jest/globals";
 import { render } from "@testing-library/react";
-
 import { BuildStatus } from "../types/buildStatus";
 import { BuildStatusChip } from "./BuildStatusChip";
 
