29799eb89470c0b768236947ed92c6667f0a8281
Pavlo Strunkin <pashidlos@gmail.com>
2023-08-11 17:14:23 +0300

    TestDetailsModal style fixed

--- src/components/BuildList/index.tsx
+++ src/components/BuildList/index.tsx
@@ -44,7 +44,7 @@ const useStyles = makeStyles(() =>
       visibility: "hidden",
     },
     listItem: {
-      paddingRight: "48px",
+      paddingRight: 48,
       "&:hover $listItemSecondaryAction": {
         visibility: "inherit",
       },
