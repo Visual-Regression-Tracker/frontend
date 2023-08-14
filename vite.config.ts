09f136a4b1d6c521e7666c2144739167373db468
Pavlo Strunkin <pashidlos@gmail.com>
2023-08-14 13:49:53 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -44,8 +44,4 @@ export default defineConfig({
         }
         */
   },
-  define: {
-    // Some libraries use the global object, even though it doesn't exist in the browser.
-    global: {},
-  },
 });
