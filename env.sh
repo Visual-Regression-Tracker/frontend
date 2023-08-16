1a254b0bece5ecd6c1607d882503186d85e102e4
Pavlo Strunkin <pashidlos@gmail.com>
2023-08-16 19:07:16 +0300

    Fix ENV variables are not being set inside docker
    closes https://github.com/Visual-Regression-Tracker/Visual-Regression-Tracker/issues/414

---
+++ env.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+# (c) https://github.com/kunokdev/cra-runtime-environment-variables/blob/master/env.sh
+# line endings must be \n, not \r\n !
+
+echo "window._env_ = {" > ./env-config.js
+awk -F '=' '{ print $1 ": \"" (ENVIRON[$1] ? ENVIRON[$1] : $2) "\"," }' ./.env >> ./env-config.js
+echo "}" >> ./env-config.js
