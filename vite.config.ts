b48be4fa32dc8709c569fe243563888214bbde92
Juga Paazmaya <paazmaya@yahoo.com>
2024-01-16 17:54:25 +0200

    Remove manual chunks and disable inlining assets (#351)
    * Starting to fine tune manual chunks
    
    * Using the smallest preset for treeshaking with side effects true
    
    * Just MUI?
    
    * Forget about manual chunks and other Rollup tuning, just makes things worse

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,5 @@
 import { defineConfig } from "vite";
-
 import react from "@vitejs/plugin-react";
-
-/*
-Saved for later use once other libraries are being updated.
-function manualChunks(id : string) {
-    if (id.includes('node_modules/react')) {
-        return 'react';
-    } else if (id.includes('node_modules/@material-ui')) {
-        return 'material';
-    } else if (id.includes('node_modules')) {
-        return 'vendor';
-    }
-}
-*/
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -31,17 +17,10 @@ export default defineConfig({
   build: {
     outDir: "build",
     sourcemap: true,
+    assetsInlineLimit: 0, // disable inlining assets
 
     // When false, all CSS will be extracted into a single CSS file.
     cssCodeSplit: false,
 
-    // https://rollupjs.org/configuration-options/
-    /*
-        rollupOptions: {
-            output: {
-                manualChunks: manualChunks
-            }
-        }
-        */
   },
 });
