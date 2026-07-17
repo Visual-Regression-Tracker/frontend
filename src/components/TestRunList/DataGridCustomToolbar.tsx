4819a511c010ace917e12a9a425f01d17f79c9e0
Mykola Gervasyuk <gervasyuk@gmail.com>
2026-07-17 21:50:24 +0300

    feat: multi-value, combinable Name/Tag/Status filters for test runs (#377)

--- src/components/TestRunList/DataGridCustomToolbar.tsx
+++ src/components/TestRunList/DataGridCustomToolbar.tsx
@@ -1,15 +1,11 @@
 import React from "react";
 import { Toolbar, Box } from "@mui/material";
-import {
-  GridToolbarDensitySelector,
-  GridToolbarFilterButton,
-} from "@mui/x-data-grid";
+import { GridToolbarDensitySelector } from "@mui/x-data-grid";
 import { BulkOperation } from "./BulkOperation";
 
 export const DataGridCustomToolbar: React.FunctionComponent = () => (
   <React.Fragment>
     <Toolbar variant="dense">
-      <GridToolbarFilterButton />
       <GridToolbarDensitySelector />
       <Box marginLeft="auto">
         <BulkOperation />
